a8b1eb6ecf08771f3d024f36f612640a9606bc84
Jeonguk <skcju12@gmail.com>
2025-10-24 02:00:40 +0900

    fix: 루트 경로

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/portfolio/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
